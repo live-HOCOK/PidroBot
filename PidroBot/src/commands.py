c42f2194230b9e43666ee2fb5e4e623619224b2f
live-HOCOK <zaordy@gmail.com>
2021-01-16 01:13:48 +0300

    add statistic * добавлена статистика * добавлен счет рейтинга

--- PidroBot/src/commands.py
+++ PidroBot/src/commands.py
@@ -7,10 +7,16 @@ def pidro(message, db: Database):
     mention_user_name = get_mention(message)
     if not mention_user_name:
         return constants.ERROR_USERNAME_NOT_FOUND
-    pidro = 0
+    pidro = db.add_pidro(message.chat.id, mention_user_name)
     answer = constants.ANSWER_ADD_PIDRO.format(username=mention_user_name, pidro=pidro)
     return answer
 
 
-def stats(message, db):
-    pass
+def stats(message, db: Database):
+    df_statistic = db.get_stat(message.chat.id)
+    answer = ''
+    if df_statistic.empty:
+        answer = constants.ERROR_EMPTY_STATS
+    for _, row in df_statistic.iterrows():
+        answer += row['user_name'] + '\t' + str(row['rating']) + '\n'
+    return answer
--- PidroBot/src/database.py
+++ PidroBot/src/database.py
@@ -7,7 +7,15 @@ class Database:
         self.df = pd.DataFrame(columns=['chat_id', 'user_name', 'rating'])
 
     def add_pidro(self, chat_id: str, user_name: str):
-        pass
+        if self.df[(self.df.chat_id == chat_id) & (self.df.user_name == user_name)].empty:
+            self.df.loc[len(self.df)] = [chat_id, user_name, 0]
+        self.df.loc[(self.df['chat_id'] == chat_id) & (self.df['user_name'] == user_name), 'rating'] += 1
+        rating = self.df[(self.df.chat_id == chat_id) & (self.df.user_name == user_name)].rating
+        return rating.loc[rating.index[0]]
 
-    def get_pidro(self, chat_id: str, user_name: str):
-        pass
+    def get_stat(self, chat_id: str):
+        result = pd.DataFrame()
+        if self.df[self.df.chat_id == chat_id].empty:
+            return result
+        result = self.df[self.df.chat_id == chat_id]
+        return result
