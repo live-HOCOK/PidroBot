d018b38c08d04dbcdfae308153e336c47886d449
live-HOCOK <zaordy@gmail.com>
2021-01-25 18:40:15 +0300

    config heroku token

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,7 @@ import os
 from boto.s3.connection import S3Connection
 
 
-TOKEN = S3Connection(os.environ.get('PIDROBOT_TOKEN'))
+TOKEN = os.environ.get('PIDROBOT_TOKEN')
 
 ANSWER_ADD_PIDRO = '{username} твой PIDRO-рейтинг {pidro}'
 ANSWER_STATS = '{number}. {username} - {rating} \n'
