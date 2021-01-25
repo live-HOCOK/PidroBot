01d03ca8cf4f2f0eb9aa53533da0bb90187037da
live-HOCOK <zaordy@gmail.com>
2021-01-25 15:12:21 +0300

    remake structure

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,6 @@
 TOKEN = '1200709904:AAG4DdTXOpWodcj4wKFe3pLjND0MW3p80vw'
 
-ANSWER_ADD_PIDRO = '{username} твой рейтинг {pidro}'
+ANSWER_ADD_PIDRO = '{username} твой PIDRO-рейтинг {pidro}'
 ANSWER_STATS = '{number}. {username} - {rating} \n'
 
 ERROR_USERNAME_NOT_FOUND = 'В твоем сообщении мало смысла'
